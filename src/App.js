11858540ab78778fe0d2b4efa906dcec8b2e2dfa
IsaqueMansur <91393713+IsaqueMansur@users.noreply.github.com>
2022-09-12 17:18:35 -0300

    history atualizado para useNavigate

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
 
-import Login from './pages/Login';
 import GlobalStyles from './styles/GlobalStyles';
 import Header from './components/Header';
+import Routers from './Routes';
 
 function App() {
   return (
-    <>
+    <BrowserRouter>
       <Header />
-      <Login />
+      <Routers />
       <GlobalStyles />
-    </>
+    </BrowserRouter>
   );
 }
 
